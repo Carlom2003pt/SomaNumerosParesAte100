74c070d2d890aaed5e2ea75deeb7480acee9dd74
Carlom2003pt <carlom634@gmail.com>
2022-03-05 17:48:18 +0000

    Soma de todos os Numeros Pares até 100

---
+++ 525222/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace _525222
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Programa para soma de numeros pares  de 0 a 100");
+
+            //variaveis
+            int soma = 0;
+            
+
+             for (int i = 2; i <= 100; i= i + 2)
+                soma = soma + i;
+            Console.WriteLine("A soma dos numeros pares até 100 é: " + soma);
+            Console.WriteLine();
+            Console.Read();
+           
+
+
+
+           
+            
+               
+
+
+
+            
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
+
+        
+    
+
